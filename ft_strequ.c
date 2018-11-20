f3e31c47ad2c79e6223cec2932ba796f193a730a
than <pierrethan@hotmail.com>
2018-11-20 05:55:54 +0100

    strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -2,8 +2,6 @@
 
 int	ft_strequ(const char *s1, const char *s2)
 {
-	if (s1 == NULL && s2 == NULL)
-		return (1);
 	if (s1 == NULL || s2 == NULL)
 		return (0);
 	while (*s1 && *s2)
@@ -13,7 +11,7 @@ int	ft_strequ(const char *s1, const char *s2)
 		s2++;
 		s1++;
 	}
-	if (s1 == NULL && s2 == NULL)
-		return (1);
-	return (0);
+	if (*s1 != *s2)
+		return (0);
+	return (1);
 }
