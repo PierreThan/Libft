35cc98386ff806c0fb148c6f38d72d4cee0cbb3a
than <pierrethan@hotmail.com>
2018-11-21 17:16:28 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,26 +15,17 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	n;
+	size_t	i;
+	size_t	s;
 
+	i = 0;
 	n = 0;
-	if (src && dst && size)
-	{
-		while (*dst)
-		{
-			dst++;
-			n++;
-		}
-		while ((size - 1) > n && *src)
-		{
-			*dst = (char)(*src);
-			dst++;
-			src++;
-			size--;
-		}
-		while (n++ < size)
-		{
-			*dst = '\0';
-			dst++;
-		}
-	}
+	s = ft_strlen(src);
+	while (n < size && dst[n])
+		n++;
+	while ((size - 1) > n && src[i])
+		dst[n++] = (char)(src[i++]);
+	while (n < size)
+		dst[n] = '\0';
+	return (n + s - i);
 }
